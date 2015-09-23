b4fe2611c5da7293295ef3ccfacd91f6b5eaf7a0
Bo Pan <bopan@linkedin.com>
2015-09-22 22:45:04 -0700

    started my own playground

---
+++ answers/src/main/scala/playground/main/Runner.scala
@@ -0,0 +1,10 @@
+package playground.main
+
+/**
+ * Created by bopan on 9/22/15.
+ */
+object Runner {
+  def main(args: Array[String]): Unit = {
+    println("hello world!")
+  }
+}
---
+++ answers/src/main/scala/playground/main/queues/SlowAppendQueue.scala
@@ -0,0 +1,8 @@
+package playground.main.queues
+
+/**
+ * Created by bopan on 9/22/15.
+ */
+class SlowAppendQueue {
+
+}
